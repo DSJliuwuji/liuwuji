a1a17f59433f60902176a3b7ab0057a4d2c586a7
Dell <1144566048@qq.com>
2020-03-09 17:58:47 +0800

    bean

---
+++ jcart-administration-back/src/main/java/io/liuwuji/jcartadministrationback/bean/BeanConfig.java
@@ -0,0 +1,23 @@
+package io.liuwuji.jcartadministrationback.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.SecureRandom;
+
+@Configuration
+public class BeanConfig {
+
+
+
+
+
+
+
+    
+    @Bean
+    public SecureRandom secureRandom(){
+        return new SecureRandom();
+    }
+
+}
